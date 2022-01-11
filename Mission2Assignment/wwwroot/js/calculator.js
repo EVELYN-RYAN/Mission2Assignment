d937938b3c08ff04aabfdce931a9f25ff65f2f04
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2022-01-11 16:38:40 -0700

    comments

--- Mission2Assignment/wwwroot/js/calculator.js
+++ Mission2Assignment/wwwroot/js/calculator.js
@@ -1,14 +1,16 @@
 ﻿$("#grade").click(function () {
-	
+	//start by declaring variables and assigning values from inputs
     var asmt = $("#pts1").val();
 	var gp = $("#pts2").val();
 	var qz = $("#pts3").val();
 	var ex = $("#pts4").val();
 	var itx = $("#pts5").val();
 	var letter ="";
-	
+
+	//calculate the total from the given weights and variables
 	var total = (asmt * .55)+(gp * .05)+(qz * .10)+(ex * .20)+(itx * .10);
-	
+
+	//If to assign letter grade
     if(total >= 94){
 		letter = "A"
 	}else if(total >= 90){
